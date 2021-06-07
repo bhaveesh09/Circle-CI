2abd94a8139ddc4031a963ac64f620e5d435b61e
Saibhaveesh Beemireddy <beemires@flip2.engr.oregonstate.edu>
2021-06-06 20:19:39 -0700

    added test for multiple

--- test_calculator.py
+++ test_calculator.py
@@ -8,5 +8,8 @@ class testCalculatorApp:
     def test_add(self):
         assert 5 == calculator.add(3,2)
 
-    def test_add(self):
-        assert 5 == calculator.sub(10,5)
+    def test_sub(self):
+        assert 5 == calculator.subtract(10,5)
+
+    def test_multiply(self):
+        assert 4 == calculator.multiply(2,2)
